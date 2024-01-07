6ecef561aabb231e3859aa3f7078a10bc344e245
baltazarparra <baltazarparra@outlook.com>
2024-01-07 08:47:47 -0300

    First commit

---
+++ src/main/java/com/example/first/controllers/ReponseEntity.java
@@ -0,0 +1,4 @@
+package com.example.first.controllers;
+
+public class ReponseEntity<T> {
+}
---
+++ src/main/java/com/example/first/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.first.repositories;
+
+import com.example.first.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
